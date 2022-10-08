eac08920ad16f4840082d85955a49946dc49ea4a
jpldcarvalho <me@jpldcarvalho.com>
2022-10-08 14:37:51 +0100

    more fixing on v86

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,23 @@
 
-import { V86Starter } from "./lib/v86/libv86";
+import { V86Starter } from "v86";
 
-new V86Starter({
-  wasm_path: "./lib/v86/v86.wasm",
-  memory_size: 512 * 1024 * 1024,
-  vga_memory_size: 8 * 1024 * 1024,
-  screen_container: document.getElementById("screen_container"),
-  bios: {
-    url: "./lib/v86/bios/seabios.bin",
-  },
-  vga_bios: {
-    url: "./lib/v86/bios/vgabios.bin",
-  },
-  filesystem: {
-    baseurl: "./lib/v86/images/arch/",
-    basefs: "./lib/v86/images/fs.json",
-  },
-  autostart: true,
-  bzimage_initrd_from_filesystem: true,
-  cmdline: [
-    "rw",
-    "root=host9p rootfstype=9p rootflags=trans=virtio,cache=loose",
-    "init=/usr/bin/init-openrc",
-  ].join(" "),
-});
+const render = () => {
+  new V86Starter({
+    wasm_path: "./lib/v86/v86.wasm",
+    memory_size: 512 * 1024 * 1024,
+    vga_memory_size: 8 * 1024 * 1024,
+    screen_container: document.getElementById("screen_container"),
+    bios: {
+      url: "./lib/v86/bios/seabios.bin",
+    },
+    vga_bios: {
+      url: "./lib/v86/bios/vgabios.bin",
+    },
+    cdrom: {
+      url: "./lib/v86/images/linux.iso",
+    },
+    autostart: true,
+  });
+};
+
+render();
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,19 +3,18 @@ import * as webpack from 'webpack';
 import 'webpack-dev-server';
 
 const config: webpack.Configuration = {
-  context: path.resolve(__dirname, 'src'),
-  devtool: 'inline-source-map',
-  entry: './index.ts',
-  mode: 'development',
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src/index.ts'),
   module: {
     rules: [{
       test: /\.ts$/,
       use: 'ts-loader',
+      exclude: /node_modules/,
     }]
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist/js')
+    path: path.resolve(__dirname, 'dist', 'js')
   },
   resolve: {
     extensions: ['.ts', '.js'],
